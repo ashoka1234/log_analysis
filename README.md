4d6d9fcf1b37ff22d9728e408514561cb2558920
Ashoka Jayawardena <ashoka1234@users.noreply.github.com>
2017-11-01 21:57:31 +1100

    second commit

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 Log Analysis is a simple reporting tool of a news database which consists of articles, authors, and log tables. The tool is written in Python and the database is a PostgreSQL installed in a virtual machine. The connection to the database is via a vagrant connection to the virtual machine.
 
 # Install
-A PostgreSQL database is required which could be installed in a virtual machine. A vagrant connection could be used to connect to the virtual machine from a windows shell. The tool is in a single source file `log_analysis.py`
+A PostgreSQL database is required which could be installed in a virtual machine. A vagrant connection could be used to connect to the virtual machine from a windows shell.
+
+The database is called `news`. The tables could be created and populated by running the SQL file `newsdata.sql` in the `vagrant` directory using the following command:
+
+`psql -d news -f newsdata.sql`
+
+The tool is in a single source file `log_analysis.py`
 
 # Run
 Simply run the following Python interpreter in a shell.
--- log_analysis.py
+++ log_analysis.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import psycopg2
 
 # Connect to database news
@@ -5,48 +6,43 @@ db = psycopg2.connect('dbname=news user=vagrant')
 cur = db.cursor()
 
 print "What are the most populor three articles of all time?"
-cur.execute("SELECT articles.title, popularviews.popularity \
-             FROM (SELECT SUBSTR(log.path,10) AS slug, \
-                          COUNT(log.path) AS popularity \
-                   FROM log, articles \
-                   WHERE SUBSTR(log.path, 1, 9) = '/article/' \
-                         AND SUBSTR(log.path,10) = articles.slug \
-                   GROUP BY log.path \
-                   ORDER BY popularity DESC \
-                   LIMIT 3) AS popularviews \
-             INNER JOIN articles ON articles.slug = popularviews.slug \
-             ORDER BY popularity DESC")
+cur.execute("""SELECT articles.title, COUNT(log.path) AS popularity
+               FROM log, articles
+               WHERE CONCAT('/article/', articles.slug) = log.path
+               GROUP BY articles.title
+               ORDER BY popularity DESC
+               LIMIT 3""")
 for article in cur.fetchall():
     print "\"{}\" - {} views".format(article[0], article[1])
 
 print "\n"
 print "Who are the most popular authors of all time?"
-cur.execute("SELECT authors.name, popularauthorIds.popularity \
-             FROM (SELECT authorId, COUNT(authorId) AS popularity \
-                   FROM (SELECT articles.author AS authorId \
-                         FROM log, articles \
-                         WHERE SUBSTR(log.path, 1, 9) = '/article/' AND \
-                               SUBSTR(log.path,10) = articles.slug) \
-                         AS articleauthors \
-                   GROUP BY authorId \
-                   ORDER BY popularity DESC) AS popularauthorIds, authors \
-             WHERE popularauthorIds.authorId = authors.id")
+cur.execute("""SELECT authors.name, popularauthorIds.popularity
+               FROM (SELECT authorId, COUNT(authorId) AS popularity
+                     FROM (SELECT articles.author AS authorId
+                           FROM log, articles
+                           WHERE CONCAT('/article/', articles.slug) = log.path)
+                           AS articleauthors
+                     GROUP BY authorId
+                     ORDER BY popularity DESC) AS popularauthorIds, authors
+               WHERE popularauthorIds.authorId = authors.id""")
 for author in cur.fetchall():
     print "\"{}\" - {} views".format(author[0], author[1])
 
 print "\n"
 print "On which days did more than 1% of requests lead to errors?"
-cur.execute("SELECT COUNT(CASE WHEN SUBSTR(status,1,1) = '4' THEN 1 END) \
-                        AS error4xx, \
-                    COUNT(CASE WHEN SUBSTR(status,1,1) = '5' THEN 1 END) \
-                        AS error5xx, \
-                    COUNT(*) AS total, \
-                    time::date AS date \
-             FROM log \
-             GROUP BY date")
+cur.execute("""
+       SELECT error_pct, to_char(date, 'FMMonth FMDD, YYYY') as date
+       FROM (SELECT 100.0*(
+                    COUNT(CASE WHEN SUBSTR(status,1,1) = '4' THEN 1 END)
+                         +
+                    COUNT(CASE WHEN SUBSTR(status,1,1) = '5' THEN 1 END))
+                         /
+                    COUNT(*) AS error_pct, time::date AS date
+             FROM log
+             GROUP BY date) AS daily_errors
+       WHERE error_pct > 1.0""")
 for status in cur.fetchall():
-    errorratio = ((status[0] + status[1]) / float(status[2]))*100
-    if (errorratio > 1.0):
-        print "{:%b %d, %Y} - {:2.2f}% errors".format(status[3], errorratio)
+    print "{} - {:2.2f}% errors".format(status[1], status[0])
 
 db.close()
